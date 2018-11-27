f2ad7e77e04800d86ff7dc0af0b02dc8891ab854
Alusor <eddvelez@outlook.com>
2018-11-27 17:14:44 -0600

    [Add] Load option for confing

--- src/config/create-config.js
+++ src/config/create-config.js
@@ -10,6 +10,7 @@ export default (userConfig) => {
 
   combinedConfig.allLanguages = combinedConfig.otherLanguages
     .concat([combinedConfig.defaultLanguage])
+  combinedConfig.load = combinedConfig.load || 'languageOnly'
 
   if (isNode && !process.browser) {
     const fs = eval("require('fs')") // eslint-disable-line
@@ -17,11 +18,12 @@ export default (userConfig) => {
 
     const getAllNamespaces = p => fs.readdirSync(p).map(file => file.replace('.json', ''))
     const {
-      allLanguages, defaultLanguage, localePath, localeStructure,
+      allLanguages, defaultLanguage, localePath, localeStructure, load
     } = combinedConfig
 
     combinedConfig = {
       ...combinedConfig,
+      load: load,
       preload: allLanguages,
       ns: getAllNamespaces(path.join(process.cwd(), `${localePath}/${defaultLanguage}`)),
       backend: {
