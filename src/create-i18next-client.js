2f24f762dc3cd1103bfc2aa83d5743ac5669794e
Lucas Feliciano <mail@lucasfeliciano.com>
2018-11-29 10:24:54 +0100

    Add custom detectors when creating i18n instance

--- src/create-i18next-client.js
+++ src/create-i18next-client.js
@@ -1,21 +1,32 @@
 import isNode from 'detect-node'
 import i18next from 'i18next'
 import i18nextXHRBackend from 'i18next-xhr-backend'
-import i18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 
 const i18n = i18next.default ? i18next.default : i18next
 i18n.nsFromReactTree = []
 
-if (isNode) {
-  const i18nextNodeBackend = eval("require('i18next-node-fs-backend')") // eslint-disable-line
-  const i18nextMiddleware = require('i18next-express-middleware')
-  i18n.use(i18nextNodeBackend).use(i18nextMiddleware.LanguageDetector)
-} else {
-  i18n.use(i18nextXHRBackend).use(i18nextBrowserLanguageDetector)
-}
-
 export default (config) => {
   if (!i18n.isInitialized) {
+    if (isNode) {
+      const i18nextNodeBackend = eval("require('i18next-node-fs-backend')") // eslint-disable-line
+      const I18nextMiddleware = require('i18next-express-middleware')
+
+      const serverLanguageDetector = new I18nextMiddleware.LanguageDetector()
+
+      config.detection.serverDetectors.forEach((detector) => {
+        serverLanguageDetector.addDetector(detector)
+      })
+      i18n.use(i18nextNodeBackend).use(serverLanguageDetector)
+    } else {
+      const browserLanguageDetector = new I18nextBrowserLanguageDetector()
+
+      config.detection.browserDetectors.forEach((detector) => {
+        browserLanguageDetector.addDetector(detector)
+      })
+      i18n.use(i18nextXHRBackend).use(browserLanguageDetector)
+    }
+
     i18n.init(config)
   }
   return i18n
