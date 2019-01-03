0dfc49acf9a750d8e1119f1dd7d61381bea61270
Mathias <mathias@kandelborg.dk>
2019-01-03 12:30:09 +0100

    Refactor console-message.js, follow naming conventions, improve comments
    I'm not sure if this actually fixes all the CodeClimate problems but I'm almost certain.
    
    Furthermore, the new logMessage function could be extracted to its own file if that's something in interest. Until further notice, I've kept them together.

--- src/utils/console-message.js
+++ src/utils/console-message.js
@@ -1,45 +1,85 @@
 /* eslint-disable no-console */
 
-
 /**
   * @readonly
   * @enum {String} A console.log type
   */
 const messageTypes = {
-  warn: 'Warning',
-  err: 'Error',
-  info: 'Info',
+  error: 'error',
+  info: 'info',
+  warn: 'warn',
 }
 
 Object.freeze(messageTypes)
 
 /**
- * Sets the Error.stackTraceLimit to default or traceLimit
+ * Sets the Error.errorStackTraceLimit to default or errorStackTraceLimit
  * https://v8.dev/docs/stack-trace-api
- * @param {number} traceLimit
+ * @param {number} errorStackTraceLimit
  * @returns
  */
-function setStackTraceLimit(traceLimit = 10) {
-  Error.stackTraceLimit = traceLimit
+function setErrorStackTraceLimit(errorStackTraceLimit = 10) {
+  Error.stackTraceLimit = errorStackTraceLimit
 }
 
+/**
+ *  Logs a custom message to console
+ * @param {PrettyError} PrettyError
+ * @param {messageTypes} messageType One of: error, warn or info
+ * @param {String} message
+ */
+function logMessage(PrettyError, messageType, message) {
+  const capitalize = str => str.replace(str[0], str[0].toUpperCase())
+
+  const pe = new PrettyError()
+
+  // Create a new empty error
+  const newLog = new Error()
+
+  // Set the new error message
+  newLog.message = message
+
+  if (messageType === messageTypes.error) {
+    newLog.name = capitalize(messageTypes.error)
+    console.error(pe.render(newLog))
+  }
+
+  if (messageType === messageTypes.info) {
+    newLog.name = capitalize(messageTypes.info)
+    console.info(pe.render(newLog))
+  }
+
+  if (messageType === messageTypes.warn) {
+    newLog.name = capitalize(messageTypes.warn)
+    console.warn(pe.render(newLog))
+  }
+
+  if (messageType !== messageTypes.error
+    && messageType !== messageTypes.info
+    && messageType !== messageTypes.warn) {
+    newLog.name = capitalize(messageTypes.info)
+    console.info(pe.render(newLog))
+  }
+}
 
 /**
- * Create a console log with specified type, message and options
- * @param {messageTypes} messageType One of: err, warn or info
+ * Create a console log with specified log type, a message and options
+ * @param {messageTypes} messageType One of: error, warn or info
  * @param {String} message
  * @param {Object} options
  */
 export default function createConsoleLog(
-  messageType = messageTypes.info,
+  messageType,
   message,
   options = {
-    traceLimit: 0,
+    errorStackTraceLimit: 0,
     strictMode: true,
   }) {
-  const { traceLimit, strictMode } = options
+  const { errorStackTraceLimit, strictMode } = options
   let util
   let PrettyError
+  let pe
+
 
   if (!strictMode) {
     return
@@ -52,18 +92,19 @@ export default function createConsoleLog(
   if (process.env.NODE_ENV !== 'production') {
     util = require('util')
     PrettyError = require('pretty-error')
+
+    pe = new PrettyError()
+
   } else {
     return
   }
 
-  const pe = new PrettyError()
+  /* Temporarily set the stacktrace to 0 or errorStackTraceLimit,
+     in order to only display a message */
+  Error.errorStackTraceLimit = errorStackTraceLimit
 
-  /* Temporarily set the stacktrace to 0 or traceLimit, in order to only display a message */
-  Error.stackTraceLimit = traceLimit
-  /* Make room for new message */
+  // Make room for new message
   console.log()
-  // Create a new empty error
-  const newLog = new Error()
 
   // Make sure the message is a string
   if (typeof message !== 'string') {
@@ -82,28 +123,9 @@ export default function createConsoleLog(
     return
   }
 
-  // Set the new error message
-  newLog.message = message
+  // Log the message to console
+  logMessage(PrettyError, messageType, message)
 
-  switch (messageType) {
-    case 'warn':
-      newLog.name = messageTypes.warn
-      console.warn(pe.render(newLog))
-      setStackTraceLimit()
-      return
-    case 'err':
-      newLog.name = messageTypes.err
-      console.error(pe.render(newLog))
-      setStackTraceLimit()
-      return
-    case 'info':
-      newLog.name = messageTypes.info
-      console.info(pe.render(newLog))
-      setStackTraceLimit()
-      return
-    default:
-      newLog.name = messageTypes.info
-      console.info(pe.render(newLog))
-      setStackTraceLimit()
-  }
+  // Reset the Error.stackTraceLimit to 10
+  setErrorStackTraceLimit()
 }
