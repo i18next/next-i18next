7ee7bcbd777b7f9f2269175edb5cdeb713143546
Rob Capellini <rob.capellini@gmail.com>
2019-01-14 17:21:27 -0500

    Changing test

--- __tests__/components/Link.test.js
+++ __tests__/components/Link.test.js
@@ -30,29 +30,86 @@ describe('Link component', () => {
     <Link {...props} {...otherProps}>click here</Link>,
   ).find('Link').at(1)
 
-  describe('localeSubpaths === false', () => {
-    describe('href as string', () => {
-      beforeEach(() => {
-        props.href = '/foo/bar'
-      })
+  it('renders without lang if localeSubpaths === false', () => {
+    // without 'as' prop
+    let component = createLinkComponent()
 
-      it('renders without lang', () => {
-        const component = createLinkComponent()
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toBeUndefined()
 
-        expect(component.prop('href')).toEqual('/foo/bar')
-        expect(component.prop('as')).toBeUndefined()
-      })
+    // with 'as' prop
+    props.as = '/foo?bar'
+    component = createLinkComponent()
 
-      it('renders without lang (with "as" prop)', () => {
-        props.as = '/foo?bar'
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toEqual('/foo?bar')
+  })
 
-        const component = createLinkComponent()
+  it('renders without lang if props.lng is undefined', () => {
+    props.nextI18NextConfig.config.localeSubpaths = true
+    props.lng = undefined
 
-        expect(component.prop('href')).toEqual('/foo/bar')
-        expect(component.prop('as')).toEqual('/foo?bar')
-      })
-    })
+    // without 'as' prop
+    let component = createLinkComponent()
 
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toBeUndefined()
+
+    // with 'as' prop
+    props.as = '/foo?bar'
+    component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toEqual('/foo?bar')
+  })
+
+  it('renders without lang if props.lng === defaultLanguage', () => {
+    props.nextI18NextConfig.config.localeSubpaths = true
+    props.nextI18NextConfig.config.defaultLanguage = 'en'
+    props.lng = 'en'
+
+    // without 'as' prop
+    let component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toBeUndefined()
+
+    // with 'as' prop
+    props.as = '/foo?bar'
+    component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual('/foo/bar')
+    expect(component.prop('as')).toEqual('/foo?bar')
+  })
+
+  it('renders with lang', () => {
+    props.nextI18NextConfig.config.localeSubpaths = true
+    props.nextI18NextConfig.config.defaultLanguage = 'en'
+
+    // without 'as' prop -- no query parameters
+    let component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
+    expect(component.prop('as')).toEqual('/de/foo/bar')
+
+    // without 'as' prop -- query parameters
+    props.href = '/foo/bar?baz'
+    component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { baz: '', lng: 'de' } })
+    expect(component.prop('as')).toEqual('/de/foo/bar?baz')
+
+    props.href = '/foo/bar'
+
+    // with 'as' prop
+    props.as = '/foo?bar'
+    component = createLinkComponent()
+
+    expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
+    expect(component.prop('as')).toEqual('/de/foo?bar')
+  })
+
+  describe('https://github.com/isaachinman/next-i18next/issues/89', () => {
     describe('href as object', () => {
       beforeEach(() => {
         props.href = {
@@ -61,129 +118,64 @@ describe('Link component', () => {
         }
       })
 
-      it('renders without lang', () => {
-        const component = createLinkComponent()
+      describe('localeSubpaths === false', () => {
+        beforeEach(() => {
+          props.nextI18NextConfig.config.localeSubpaths = false
+        })
 
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: {} })
-        expect(component.prop('as')).toBeUndefined()
+        it('renders without lang', () => {
+          const component = createLinkComponent()
+
+          expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: {} })
+          expect(component.prop('as')).toBeUndefined()
+        })
+
+        it('renders without lang (with "as" prop)', () => {
+          props.as = '/foo?bar'
+
+          const component = createLinkComponent()
+
+          expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: {} })
+          expect(component.prop('as')).toEqual('/foo?bar')
+        })
       })
 
-      it('renders without lang (with "as" prop)', () => {
-        props.as = '/foo?bar'
+      describe('localeSubpaths === true', () => {
+        beforeEach(() => {
+          props.nextI18NextConfig.config.localeSubpaths = true
+        })
 
-        const component = createLinkComponent()
+        beforeEach(() => {
+          props.nextI18NextConfig.config.defaultLanguage = 'en'
 
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: {} })
-        expect(component.prop('as')).toEqual('/foo?bar')
-      })
-    })
-  })
+          props.href = { pathname: '/foo/bar', query: {} }
+        })
 
-  describe('localeSubpaths === true', () => {
-    beforeEach(() => {
-      props.nextI18NextConfig.config.localeSubpaths = true
-    })
+        it('renders with lang', () => {
+          const component = createLinkComponent()
 
-    it('renders without lang if props.lng is undefined', () => {
-      props.lng = undefined
+          expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
+          expect(component.prop('as')).toEqual('/de/foo/bar')
+        })
 
-      // without 'as' prop
-      let component = createLinkComponent()
+        it('renders with lang (with "as" prop)', () => {
+          props.as = '/foo?bar'
 
-      expect(component.prop('href')).toEqual('/foo/bar')
-      expect(component.prop('as')).toBeUndefined()
+          const component = createLinkComponent()
 
-      // with 'as' prop
-      props.as = '/foo?bar'
-      component = createLinkComponent()
+          expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
+          expect(component.prop('as')).toEqual('/de/foo?bar')
+        })
 
-      expect(component.prop('href')).toEqual('/foo/bar')
-      expect(component.prop('as')).toEqual('/foo?bar')
-    })
+        it('renders with lang (with query parameters)', () => {
+          props.href.query = { baz: '' }
 
-    it('renders without lang if props.lng === defaultLanguage', () => {
-      props.nextI18NextConfig.config.defaultLanguage = 'en'
-      props.lng = 'en'
+          const component = createLinkComponent()
 
-      // without 'as' prop
-      let component = createLinkComponent()
-
-      expect(component.prop('href')).toEqual('/foo/bar')
-      expect(component.prop('as')).toBeUndefined()
-
-      // with 'as' prop
-      props.as = '/foo?bar'
-      component = createLinkComponent()
-
-      expect(component.prop('href')).toEqual('/foo/bar')
-      expect(component.prop('as')).toEqual('/foo?bar')
-    })
-
-    describe('href as string', () => {
-      beforeEach(() => {
-        props.nextI18NextConfig.config.defaultLanguage = 'en'
-
-        props.href = '/foo/bar'
-      })
-
-      it('renders with lang', () => {
-        const component = createLinkComponent()
-
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo/bar')
-      })
-
-      it('renders with lang (with "as" prop)', () => {
-        props.as = '/foo?bar'
-
-        const component = createLinkComponent()
-
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo?bar')
-      })
-
-      it('renders with lang (with query parameters)', () => {
-        props.href = '/foo/bar?baz'
-
-        const component = createLinkComponent()
-
-        expect(component.prop('href'))
-          .toEqual({ pathname: '/foo/bar', query: { baz: '', lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo/bar?baz')
-      })
-    })
-
-    describe('href as an object', () => {
-      beforeEach(() => {
-        props.nextI18NextConfig.config.defaultLanguage = 'en'
-
-        props.href = { pathname: '/foo/bar', query: {} }
-      })
-
-      it('renders with lang', () => {
-        const component = createLinkComponent()
-
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo/bar')
-      })
-
-      it('renders with lang (with "as" prop)', () => {
-        props.as = '/foo?bar'
-
-        const component = createLinkComponent()
-
-        expect(component.prop('href')).toEqual({ pathname: '/foo/bar', query: { lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo?bar')
-      })
-
-      it('renders with lang (with query parameters)', () => {
-        props.href.query = { baz: '' }
-
-        const component = createLinkComponent()
-
-        expect(component.prop('href'))
-          .toEqual({ pathname: '/foo/bar', query: { baz: '', lng: 'de' } })
-        expect(component.prop('as')).toEqual('/de/foo/bar?baz=')
+          expect(component.prop('href'))
+            .toEqual({ pathname: '/foo/bar', query: { baz: '', lng: 'de' } })
+          expect(component.prop('as')).toEqual('/de/foo/bar?baz=')
+        })
       })
     })
   })
