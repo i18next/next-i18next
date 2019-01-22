5448488956f53ccf2c8d2776829c4823243fc1d5
Sudhir Shrestha <sudhirshrestha@live.com>
2019-01-23 00:02:30 +0700

    Implement lng-from-req to use req.i18n.language

--- src/utils/lng-from-req.js
+++ src/utils/lng-from-req.js
@@ -1,11 +1,9 @@
 export default (req) => {
-
   const { allLanguages, defaultLanguage, fallbackLng } = req.i18n.options
 
-  const language = req.i18n.languages.find(l => allLanguages.includes(l))
+  const language = req.i18n.languages.find(l => req.i18n.language || allLanguages.includes(l))
     || fallbackLng
     || defaultLanguage
 
   return language
-
 }
