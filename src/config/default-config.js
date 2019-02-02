5f5d9cec3797d59c781867fa52f969e6bd3a2ca5
Rob Capellini <rob.capellini@gmail.com>
2019-02-02 11:38:14 -0500

    Moving setting of config.backend to default configuration

--- src/config/default-config.js
+++ src/config/default-config.js
@@ -1,8 +1,11 @@
+import isNode from 'detect-node'
+
 export const localeSubpathOptions = {
   ALL: 'all',
   FOREIGN: 'foreign',
   NONE: 'none',
 }
+export const runningServerSide = isNode && !process.browser
 
 const DEFAULT_LANGUAGE = 'en'
 const OTHER_LANGUAGES = []
@@ -11,7 +14,7 @@ const LOCALE_PATH = 'static/locales'
 const LOCALE_STRUCTURE = '{{lng}}/{{ns}}'
 const LOCALE_SUBPATHS = localeSubpathOptions.NONE
 
-export default {
+const config = {
   defaultLanguage: DEFAULT_LANGUAGE,
   otherLanguages: OTHER_LANGUAGES,
   load: 'currentOnly',
@@ -35,13 +38,25 @@ export default {
     order: ['cookie', 'header', 'querystring'],
     caches: ['cookie'],
   },
-  backend: {
-    loadPath: `/${LOCALE_PATH}/${LOCALE_STRUCTURE}.json`,
-    addPath: `/${LOCALE_PATH}/${LOCALE_STRUCTURE}.missing.json`,
-  },
   react: {
     wait: true,
   },
   strictMode: true,
   errorStackTraceLimit: 0,
 }
+
+if (runningServerSide) {
+  const path = require('path')
+
+  config.backend = {
+    loadPath: path.join(process.cwd(), `${LOCALE_PATH}/${LOCALE_STRUCTURE}.json`),
+    addPath: path.join(process.cwd(), `${LOCALE_PATH}/${LOCALE_STRUCTURE}.missing.json`),
+  }
+} else {
+  config.backend = {
+    loadPath: `/${LOCALE_PATH}/${LOCALE_STRUCTURE}.json`,
+    addPath: `/${LOCALE_PATH}/${LOCALE_STRUCTURE}.missing.json`,
+  }
+}
+
+export default config
