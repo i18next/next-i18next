e7d13e41c3d920dda0ce8513d14810309f164c8b
Rob Capellini <rob.capellini@gmail.com>
2019-02-27 15:42:31 +0100

    Update src/utils/locale-subpath-required.js
    Co-Authored-By: dayze <elye24@gmail.com>

--- src/utils/locale-subpath-required.js
+++ src/utils/locale-subpath-required.js
@@ -1,4 +1,5 @@
 export default (nextI18NextConfig, lng) => {
   const { defaultLanguage, localeSubpaths, defaultLocaleSubpath } = nextI18NextConfig.config
+
   return localeSubpaths && lng && (lng !== defaultLanguage || defaultLocaleSubpath)
 }
