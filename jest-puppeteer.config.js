ad0d96e6b11da9c5c17523d0f1184f2e96dbd2ab
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-03-10 23:01:31 +0530

    fix: inconsistencies in jest-puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -42,17 +42,17 @@ module.exports = {
       command: `cd examples/simple && LOCALE_SUBPATHS=all PORT=${e2eTests.LOCALE_SUBPATHS_ALL.port} NODE_ENV=production node index.js`,
     },
     {
-      ...e2eTests.BASIC,
+      ...e2eTests.TS_BASIC,
       ...defaults,
       command: `cd examples/typescript && PORT=${e2eTests.TS_BASIC.port} NODE_ENV=production node .build/index.js`,
     },
     {
-      ...e2eTests.LOCALE_SUBPATHS_FOREIGN,
+      ...e2eTests.TS_LOCALE_SUBPATHS_FOREIGN,
       ...defaults,
       command: `cd examples/typescript && LOCALE_SUBPATHS=foreign PORT=${e2eTests.TS_LOCALE_SUBPATHS_FOREIGN.port} NODE_ENV=production node .build/index.js`,
     },
     {
-      ...e2eTests.LOCALE_SUBPATHS_ALL,
+      ...e2eTests.TS_LOCALE_SUBPATHS_ALL,
       ...defaults,
       command: `cd examples/typescript && LOCALE_SUBPATHS=all PORT=${e2eTests.TS_LOCALE_SUBPATHS_ALL.port} NODE_ENV=production node .build/index.js`,
     },
