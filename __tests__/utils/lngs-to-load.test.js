b26fe0671580dd4da093a763d516af15757d7056
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2019-04-18 13:47:42 +0200

    linting

--- __tests__/utils/lngs-to-load.test.js
+++ __tests__/utils/lngs-to-load.test.js
@@ -19,22 +19,22 @@ describe('lngsToLoad utility function', () => {
   })
 
   it('returns locale specific fallbackLng', () => {
-    const analysis = lngsToLoad('nl', { 'nl': 'en', 'sv': 'da' })
+    const analysis = lngsToLoad('nl', { nl: 'en', sv: 'da' })
     expect(analysis).toMatchObject(['nl', 'en'])
   })
 
   it('returns locale specific fallbackLng and default', () => {
-    const analysis = lngsToLoad('fr', { 'fr': 'de', default: 'en' })
+    const analysis = lngsToLoad('fr', { fr: 'de', default: 'en' })
     expect(analysis).toMatchObject(['fr', 'de', 'en'])
   })
 
   it('returns locale specific fallbackLng array', () => {
-    const analysis = lngsToLoad('fi', { 'fi': ['dk', 'sv', 'en'] })
+    const analysis = lngsToLoad('fi', { fi: ['dk', 'sv', 'en'] })
     expect(analysis).toMatchObject(['fi', 'dk', 'sv', 'en'])
   })
 
   it('returns locale specific fallbackLng array and default', () => {
-    const analysis = lngsToLoad('fi', { 'fi': ['dk', 'en'], default: 'it' })
+    const analysis = lngsToLoad('fi', { fi: ['dk', 'en'], default: 'it' })
     expect(analysis).toMatchObject(['fi', 'dk', 'en', 'it'])
   })
 
