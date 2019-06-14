a3477cf7bad59f508ce906a51a2ede49704f8128
Isaac Hinman <isaac@isaachinman.com>
2019-06-14 09:17:15 +0100

    feat: Add useTranslation (#343)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { withTranslation, Trans } from 'react-i18next'
+import { withTranslation, useTranslation, Trans } from 'react-i18next'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 
 import createConfig from './config/create-config'
@@ -27,9 +27,12 @@ export default class NextI18Next {
       withTranslation(namespace, options)(Component), Component)
 
     const nextI18NextInternals = { config: this.config, i18n: this.i18n }
-    this.Trans = Trans
     this.Link = withInternals(Link, nextI18NextInternals)
     this.Router = wrapRouter(nextI18NextInternals)
+
+    /* Directly export `react-i18next` methods */
+    this.Trans = Trans
+    this.useTranslation = useTranslation
   }
 
 }
