7bf7d1f0de792490d2f65344f906a660d3b3e145
Ricardo Abreu <ricardo.abreu@lush.co.uk>
2019-06-23 12:00:08 +0100

    fix namespacesRequired undefined

--- examples/simple/pages/_app.js
+++ examples/simple/pages/_app.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import App, { Container } from 'next/app'
-import { appWithTranslation } from '../i18n'
+import App, { Container } from 'next/app';
+import React from 'react';
+import { appWithTranslation } from '../i18n';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  
   render() {
     const { Component, pageProps } = this.props
     return (
