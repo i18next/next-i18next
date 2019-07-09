6a81dbd6b3eb76dea58af48a6a85ad6b62efff85
Isaac Hinman <isaac@isaachinman.com>
2019-07-09 13:08:44 +0100

    chore: Upgrade NextJs to v9

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,6 +1,6 @@
 
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-const NextI18Next = require('next-i18next/dist/commonjs')
+const NextI18Next = require('next-i18next').default
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
