2cd59f6f7dc676a5a650aadd5b9dc3b81953042a
Isaac Hinman <isaac@isaachinman.com>
2019-07-16 19:22:27 +0100

    refactor: Convert simple example to functional components and add styling

--- examples/simple/components/Footer.js
+++ examples/simple/components/Footer.js
@@ -3,14 +3,9 @@ import PropTypes from 'prop-types'
 
 import { withTranslation } from '../i18n'
 
-class Footer extends React.Component {
-  render() {
-    const { t } = this.props
-    return (
-      <footer>{t('description')}</footer>
-    )
-  }
-}
+const Footer = ({ t }) => (
+  <footer>{t('description')}</footer>
+)
 
 Footer.propTypes = {
   t: PropTypes.func.isRequired,
--- examples/simple/pages/index.js
+++ examples/simple/pages/index.js
@@ -3,21 +3,14 @@ import PropTypes from 'prop-types'
 
 import { i18n, Link, withTranslation } from '../i18n'
 
-import Title from '../components/Title'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-class Homepage extends React.Component {
-  static async getInitialProps() {
-    return {
-      namespacesRequired: ['common', 'footer'],
-    }
-  }
-
-  render() {
-    const { t } = this.props
-    return (
-      <React.Fragment>
-        <Title />
+const Homepage = ({ t }) => (
+  <React.Fragment>
+    <main>
+      <Header title={t('h1')} />
+      <div>
         <button
           type='button'
           onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en')}
@@ -31,11 +24,15 @@ class Homepage extends React.Component {
             {t('to-second-page')}
           </button>
         </Link>
-        <Footer />
-      </React.Fragment>
-    )
-  }
-}
+      </div>
+    </main>
+    <Footer />
+  </React.Fragment>
+)
+
+Homepage.getInitialProps = async () => ({
+  namespacesRequired: ['common', 'footer'],
+})
 
 Homepage.propTypes = {
   t: PropTypes.func.isRequired,
