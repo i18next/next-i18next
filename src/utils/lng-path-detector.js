b05cc06c1a85139ae7c9074ce7ae569968aed5b7
Isaac Hinman <isaac@isaachinman.com>
2019-07-18 11:04:12 +0100

    fix: Root path redirect for localeSubpaths

--- src/utils/lng-path-detector.js
+++ src/utils/lng-path-detector.js
@@ -11,6 +11,10 @@ export default (req) => {
     const language = lngFromReq(req)
     const { allLanguages, defaultLanguage, localeSubpaths } = req.i18n.options
     let languageChanged = false
+    const languageNeedsSubpath = (localeSubpaths === localeSubpathOptions.FOREIGN
+      && language !== defaultLanguage)
+      || localeSubpaths === localeSubpathOptions.ALL
+
     /*
       If a user has hit a subpath which does not
       match their language, give preference to
@@ -22,26 +26,25 @@ export default (req) => {
         languageChanged = true
       }
     })
-    /*
-      If a user has hit the root path and their
-      language is not set to default, give
-      preference to the language and redirect
-      their path.
-    */
-    const languageNeedsSubpath = (localeSubpaths === localeSubpathOptions.FOREIGN
-      && language !== defaultLanguage)
-      || localeSubpaths === localeSubpathOptions.ALL
 
+    /*
+      If a language subpath is required and
+      not present, remove all other potential
+      subpaths and then prepend correct subpath
+    */
     if (!languageChanged && languageNeedsSubpath && !req.url.startsWith(`/${language}/`)) {
-      allLanguages.forEach((lng) => {
-        if (req.url.startsWith(`/${lng}/`)) {
-          config.correctedUrl = req.url.replace(`/${lng}/`, '/')
+      allLanguages.forEach((otherLng) => {
+        if (req.url.startsWith(`/${otherLng}/`)) {
+          config.correctedUrl = req.url.replace(`/${otherLng}/`, '/')
         }
       })
+      config.correctedUrl = req.url.replace('/', `/${language}/`)
     }
+
     /*
       If a user has a default language prefix
-      in their URL, strip it.
+      in their URL, and config option isn't
+      set to ALL, strip it.
     */
     if (language === defaultLanguage
         && req.url.startsWith(`/${defaultLanguage}/`)
