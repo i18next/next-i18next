ca483bf41a642a385eeae6f247b012fb2906697e
Isaac Hinman <isaac@isaachinman.com>
2019-09-07 16:58:43 +0100

    fix: Modify middleware approach to fix 404s

--- src/middlewares/next-i18next-middleware.ts
+++ src/middlewares/next-i18next-middleware.ts
@@ -46,41 +46,32 @@ export default function (nexti18next) {
     This does the locale subpath work
   */
   middleware.push((req: Request, res: Response, next: NextFunction) => {
-    if (isI18nRoute(req) && req.i18n && subpathIsRequired(config, req.i18n.language)) {
-      const lng = lngFromReq(req)
-      const currentLngSubpath = subpathFromLng(config, lng)
+    if (isI18nRoute(req) && req.i18n) {
+      const currentLng = lngFromReq(req)
+      const currentLngSubpath = subpathFromLng(config, currentLng)
+      const currentLngRequiresSubpath = subpathIsRequired(config, currentLng)
+      const currentLngSubpathIsPresent = subpathIsPresent(req.url, currentLngSubpath)
 
-      /*
-        This case will be entered if a subpath
-        is required but not present
-      */
-      if (!subpathIsPresent(req.url, currentLngSubpath)) {
+      const lngFromCurrentSubpath = allLanguages.find((l: string) =>
+        subpathIsPresent(req.url, subpathFromLng(config, l)))
 
-        const otherSubpathPresent = allLanguages.some((l: string) =>
-          subpathIsPresent(req.url, subpathFromLng(config, l)))
+      if (lngFromCurrentSubpath !== undefined && lngFromCurrentSubpath !== currentLng) {
 
-        if (otherSubpathPresent) {
+        /*
+          If a user has hit a subpath which does not
+          match their language, give preference to
+          the path, and change user language.
+        */
+        req.i18n.changeLanguage(lngFromCurrentSubpath)
 
-          /*
-            If a user has hit a subpath which does not
-            match their language, give preference to
-            the path, and change user language.
-          */
-          allLanguages.forEach((l: string) => {
-            if (subpathIsPresent(req.url, subpathFromLng(config, l))) {
-              req.i18n.changeLanguage(l)
-            }
-          })
+      } else if (currentLngRequiresSubpath && !currentLngSubpathIsPresent) {
 
-        } else {
+        /*
+          If a language subpath is required and
+          not present, prepend correct subpath
+        */
+        return redirectWithoutCache(res, addSubpath(req.url, currentLngSubpath))
 
-          /*
-            If a language subpath is required and
-            not present, prepend correct subpath
-          */
-          return redirectWithoutCache(res, addSubpath(req.url, currentLngSubpath))
-        }
-        
       }
 
       /*
@@ -91,7 +82,7 @@ export default function (nexti18next) {
       const params = localeSubpathRoute(req.url)
       if (params !== false) {
         const { subpath } = params
-        req.query = { ...req.query, subpath, lng }
+        req.query = { ...req.query, subpath, lng: currentLng }
         req.url = removeSubpath(req.url, subpath)
       }
     }
