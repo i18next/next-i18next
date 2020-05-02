9eb6add2651fd9a2f36d2af6e20163ad2214e715
Isaac Hinman <isaac@isaachinman.com>
2020-05-02 14:25:51 +0100

    fix: i18next-http-backend/cjs (#695)

--- src/create-i18next-client.ts
+++ src/create-i18next-client.ts
@@ -1,6 +1,6 @@
 import isNode from 'detect-node'
 import i18n from 'i18next'
-import i18nextHTTPBackend from 'i18next-http-backend'
+import i18nextHTTPBackend from 'i18next-http-backend/cjs'
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 
 import { InitPromise } from '../types'
