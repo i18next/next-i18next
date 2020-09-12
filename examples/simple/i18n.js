cc70297fe0080a43eaaf0a097d6564881d139b2f
Tim Costa <tim@timcosta.io>
2020-09-12 16:02:36 -0400

    fix: sample i18n.js file throws error
    Using

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,9 +1,10 @@
 const NextI18Next = require('next-i18next').default
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-const path = require('path')
+const Path = require('path')
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
-  localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localeSubpaths, {
+    de: 'de'
+  },
+  localePath: Path.resolve('./public/static/locales')
 })
