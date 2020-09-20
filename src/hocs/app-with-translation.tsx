2004780b3a76c7ec438399fb04ef62c1864afad1
Darkweak <sylvain@les-tilleuls.coop>
2020-09-20 23:04:18 +0200

    Update nextjs version and use getStaticProps and gerServerProps methods

--- src/hocs/app-with-translation.tsx
+++ src/hocs/app-with-translation.tsx
@@ -67,7 +67,7 @@ export const appWithTranslation = function (WrappedComponent) {
       }
     }
 
-    static async getInitialProps(ctx) {
+    static async getServerSideProps(ctx) {
 
       await initPromise
 
@@ -89,15 +89,140 @@ export const appWithTranslation = function (WrappedComponent) {
         }
       }
 
-      /* Call getInitialProps on our wrapped _app */
+      /* Call getServerSideProps on our wrapped _app */
       let wrappedComponentProps: WrappedComponentProps = { pageProps: {} }
-      if (WrappedComponent.getInitialProps) {
-        wrappedComponentProps = await WrappedComponent.getInitialProps(ctx)
+      if (WrappedComponent.getServerSideProps) {
+        wrappedComponentProps = await WrappedComponent.getServerSideProps(ctx)
       }
       if (typeof wrappedComponentProps.pageProps === 'undefined') {
         consoleMessage(
           'error',
-          'If you have a getInitialProps method in your custom _app.js file, you must explicitly return pageProps. For more information, see: https://github.com/zeit/next.js#custom-app',
+          'If you have a getServerSideProps method in your custom _app.js file, you must explicitly return pageProps. For more information, see: https://github.com/zeit/next.js#custom-app',
+        )
+      }
+
+      /*
+        Step 1: Determine initial language
+      */
+      if (req && req.i18n) {
+
+        initialLanguage = lngFromReq(req)
+
+        /*
+          Perform a lang change in case we're not on the right lang
+        */
+        await req.i18n.changeLanguage(initialLanguage)
+
+      } else if (Array.isArray(i18n.languages) && i18n.languages.length > 0) {
+        initialLanguage = i18n.language
+      }
+
+      /*
+        Step 2: Determine namespace dependencies
+      */
+      let namespacesRequired = config.ns
+      if (Array.isArray(wrappedComponentProps.pageProps.namespacesRequired)) {
+        ({ namespacesRequired } = wrappedComponentProps.pageProps)
+      } else {
+        consoleMessage(
+          'warn',
+          `You have not declared a namespacesRequired array on your page-level component: ${ctx.Component.displayName || ctx.Component.name || 'Component'}. This will cause all namespaces to be sent down to the client, possibly negatively impacting the performance of your app. For more info, see: https://github.com/isaachinman/next-i18next#4-declaring-namespace-dependencies`,
+        )
+      }
+
+      /*
+        We must always send down the defaultNS, otherwise
+        the client will trigger a request for it and issue
+        the "Did not expect server HTML to contain a <h1> in <div>"
+        error
+      */
+      if (typeof config.defaultNS === 'string' && !namespacesRequired.includes(config.defaultNS)) {
+        namespacesRequired.push(config.defaultNS)
+      }
+
+      /*
+        Step 3: Perform data fetching, depending on environment
+      */
+      if (req && req.i18n) {
+
+        /*
+          Detect the languages to load based upon the fallbackLng configuration
+        */
+        const { fallbackLng } = config
+        const languagesToLoad = lngsToLoad(initialLanguage, fallbackLng, config.otherLanguages)
+
+        /*
+          Initialise the store with the languagesToLoad and
+          necessary namespaces needed to render this specific tree
+        */
+        languagesToLoad.forEach((lng) => {
+          initialI18nStore[lng] = {}
+          namespacesRequired.forEach((ns) => {
+            initialI18nStore[lng][ns] = (
+              (req.i18n.services.resourceStore.data[lng] || {})[ns] || {}
+            )
+          })
+        })
+      } else if (Array.isArray(i18n.languages) && i18n.languages.length > 0) {
+
+        /*
+          Load newly-required translations if changing route clientside
+        */
+        await clientLoadNamespaces(i18n.languages[0], namespacesRequired)
+
+        initialI18nStore = i18n.store.data
+      }
+
+      /*
+        Step 4: Overwrite i18n.toJSON method to be able to serialize the instance
+      */
+      if (req && req.i18n) {
+        req.i18n.toJSON = () => null
+        i18nServerInstance = req.i18n
+      }
+
+      /*
+        `pageProps` will get serialized automatically by NextJs
+      */
+      return {
+        initialI18nStore,
+        initialLanguage,
+        i18nServerInstance,
+        ...wrappedComponentProps,
+      }
+    }
+
+    static async getStaticProps(ctx) {
+
+      await initPromise
+
+      /*
+        Initiate vars to return
+      */
+      const { req, res } = ctx.ctx
+      let initialI18nStore = {}
+      let initialLanguage = null
+      let i18nServerInstance = null
+
+      /*
+        Preprocess the req via next-i18next and i18next middlewares
+      */
+      if (req && res) {
+        const middlewares = nextI18NextMiddleware(nextI18Next)
+        for (const middleware of middlewares) {
+          await new Promise((resolve) => middleware(req, res, resolve))
+        }
+      }
+
+      /* Call getStaticProps on our wrapped _app */
+      let wrappedComponentProps: WrappedComponentProps = { pageProps: {} }
+      if (WrappedComponent.getStaticProps) {
+        wrappedComponentProps = await WrappedComponent.getStaticProps(ctx)
+      }
+      if (typeof wrappedComponentProps.pageProps === 'undefined') {
+        consoleMessage(
+          'error',
+          'If you have a getStaticProps method in your custom _app.js file, you must explicitly return pageProps. For more information, see: https://github.com/zeit/next.js#custom-app',
         )
       }
 
@@ -212,7 +337,7 @@ export const appWithTranslation = function (WrappedComponent) {
   }
 
   return hoistNonReactStatics(
-    withRouter(AppWithTranslation), WrappedComponent, { getInitialProps: true },
+    withRouter(AppWithTranslation), WrappedComponent, { getStaticProps: true, getServerSideProps: true },
   )
 
 }
