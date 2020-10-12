d1615c23e8f195f102749229fc5a2fa734323c7d
Andrés Franco <hello@andresfran.co>
2020-10-12 10:39:01 +0200

    refactor: prevents nested conditions

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -40,77 +40,79 @@ export const createConfig = (userConfig) => {
    * Skips translation file resolution while in cimode
    * https://github.com/isaachinman/next-i18next/pull/851#discussion_r503113620
   */
-  if (!isCIMode(defaultLanguage)) {
-    if (isServer()) {
+  if (isCIMode(defaultLanguage)) {
+    return combinedConfig
+  }
 
-      const fs = eval("require('fs')")
-      const path = require('path')
-      let serverLocalePath = localePath
+  if (isServer()) {
 
-      /*
+    const fs = eval("require('fs')")
+    const path = require('path')
+    let serverLocalePath = localePath
+
+    /*
       Validate defaultNS
       https://github.com/isaachinman/next-i18next/issues/358
     */
-      if (typeof combinedConfig.defaultNS === 'string') {
-        const defaultFile = `/${defaultLanguage}/${combinedConfig.defaultNS}.${localeExtension}`
-        const defaultNSPath = path.join(localePath, defaultFile)
-        const defaultNSExists = fs.existsSync(defaultNSPath)
-        if (!defaultNSExists) {
+    if (typeof combinedConfig.defaultNS === 'string') {
+      const defaultFile = `/${defaultLanguage}/${combinedConfig.defaultNS}.${localeExtension}`
+      const defaultNSPath = path.join(localePath, defaultFile)
+      const defaultNSExists = fs.existsSync(defaultNSPath)
+      if (!defaultNSExists) {
 
-          /*
+        /*
           If defaultNS doesn't exist, try to fall back to the deprecated static folder
           https://github.com/isaachinman/next-i18next/issues/523
         */
-          const staticDirPath = path.resolve(process.cwd(), STATIC_LOCALE_PATH, defaultFile)
-          const staticDirExists = fs.existsSync(staticDirPath)
+        const staticDirPath = path.resolve(process.cwd(), STATIC_LOCALE_PATH, defaultFile)
+        const staticDirExists = fs.existsSync(staticDirPath)
 
-          if (staticDirExists) {
-            consoleMessage('warn', 'next-i18next: Falling back to /static folder, deprecated in next@9.1.*', combinedConfig)
-            serverLocalePath = STATIC_LOCALE_PATH
-          } else if (process.env.NODE_ENV !== 'production') {
-            throw new Error(`Default namespace not found at ${defaultNSPath}`)
-          }
+        if (staticDirExists) {
+          consoleMessage('warn', 'next-i18next: Falling back to /static folder, deprecated in next@9.1.*', combinedConfig)
+          serverLocalePath = STATIC_LOCALE_PATH
+        } else if (process.env.NODE_ENV !== 'production') {
+          throw new Error(`Default namespace not found at ${defaultNSPath}`)
         }
       }
+    }
 
-      /*
+    /*
       Set server side backend
     */
-      combinedConfig.backend = {
-        loadPath: path.resolve(process.cwd(), `${serverLocalePath}/${localeStructure}.${localeExtension}`),
-        addPath: path.resolve(process.cwd(), `${serverLocalePath}/${localeStructure}.missing.${localeExtension}`),
-      }
+    combinedConfig.backend = {
+      loadPath: path.resolve(process.cwd(), `${serverLocalePath}/${localeStructure}.${localeExtension}`),
+      addPath: path.resolve(process.cwd(), `${serverLocalePath}/${localeStructure}.missing.${localeExtension}`),
+    }
 
-      /*
+    /*
       Set server side preload (languages and namespaces)
     */
-      combinedConfig.preload = allLanguages
-      if (!combinedConfig.ns) {
-        const getAllNamespaces = p => fs.readdirSync(p).map(file => file.replace(`.${localeExtension}`, ''))
-        combinedConfig.ns = getAllNamespaces(path.resolve(process.cwd(), `${serverLocalePath}/${defaultLanguage}`))
-      }
+    combinedConfig.preload = allLanguages
+    if (!combinedConfig.ns) {
+      const getAllNamespaces = p => fs.readdirSync(p).map(file => file.replace(`.${localeExtension}`, ''))
+      combinedConfig.ns = getAllNamespaces(path.resolve(process.cwd(), `${serverLocalePath}/${defaultLanguage}`))
+    }
 
-    } else {
+  } else {
 
-      let clientLocalePath = localePath
+    let clientLocalePath = localePath
     
-      /*
+    /*
       Remove public prefix from client site config
     */
-      if (localePath.startsWith('/public/')) {
-        clientLocalePath = localePath.replace(/^\/public/, '')
-      }
+    if (localePath.startsWith('/public/')) {
+      clientLocalePath = localePath.replace(/^\/public/, '')
+    }
 
-      /*
+    /*
       Set client side backend
     */
-      combinedConfig.backend = {
-        loadPath: `${clientLocalePath}/${localeStructure}.${localeExtension}`,
-        addPath: `${clientLocalePath}/${localeStructure}.missing.${localeExtension}`,
-      }
-
-      combinedConfig.ns = [combinedConfig.defaultNS]
+    combinedConfig.backend = {
+      loadPath: `${clientLocalePath}/${localeStructure}.${localeExtension}`,
+      addPath: `${clientLocalePath}/${localeStructure}.missing.${localeExtension}`,
     }
+
+    combinedConfig.ns = [combinedConfig.defaultNS]
   }
 
   /*
