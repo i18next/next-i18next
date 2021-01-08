45e54ad73b4adcab7954247578c4138537da688b
Tobias Koller <tobias.koller@protonmail.com>
2021-01-08 16:30:13 +0100

    remove serverSideTranslations from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 export { I18nContext, Trans, useTranslation, withTranslation } from 'react-i18next'
 
+<<<<<<< HEAD
 export { appWithTranslation } from './appWithTranslation'
 
+=======
+export { appWithTranslation } from './appWithTranslation'
+>>>>>>> f4d58e9 (remove serverSideTranslations from index)
