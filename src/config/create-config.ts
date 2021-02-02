b5f341889ccadd6f85196f9c33b5028ed706bf1c
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2021-02-02 21:15:32 +0200

    Allow fallbackLng to accept `false`

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -108,7 +108,7 @@ export const createConfig = (userConfig) => {
   /*
     Set fallback language to defaultLanguage in production
   */
-  if (!userConfig.fallbackLng) {
+  if (typeof userConfig.fallbackLng !== 'boolean' && !userConfig.fallbackLng) {
     combinedConfig.fallbackLng = process.env.NODE_ENV === 'production'
       ? combinedConfig.defaultLanguage
       : false
