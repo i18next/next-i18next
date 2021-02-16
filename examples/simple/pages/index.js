4a07c22e01a624ffd6467709bdb3928d197af9dc
Yuri Gor <yuri.horus@gmail.com>
2021-02-16 17:40:31 +0100

    docs: example app fixed for SSR #945

--- examples/simple/pages/index.js
+++ examples/simple/pages/index.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
-import { i18n, Link, withTranslation } from '../i18n'
+import { Link, withTranslation } from '../i18n'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-const Homepage = ({ t }) => (
+const Homepage = ({ t, i18n }) => (
   <>
     <main>
       <Header title={t('h1')} />
