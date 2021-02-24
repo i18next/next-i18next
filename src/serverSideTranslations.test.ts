5c7c6b0a41fc663b20ffed1ef0d234901ae26bc1
Isaac Hinman <isaac@isaachinman.com>
2021-02-24 22:17:56 +0000

    fix: Restore getAllNamespaces functionality

--- src/serverSideTranslations.test.ts
+++ src/serverSideTranslations.test.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { UserConfig } from '../types'
 import { serverSideTranslations } from './serverSideTranslations'
 
 jest.mock('fs', () => ({
@@ -18,6 +19,20 @@ describe('serverSideTranslations', () => {
     await expect(serverSideTranslations(undefined))
       .rejects
       .toThrow('Initial locale argument was not passed into serverSideTranslations')
+  })
+
+  it('returns all namespaces if namespacesRequired is not provided', async () => {
+    (fs.readdirSync as jest.Mock).mockReturnValue(['one', 'two', 'three'])
+    const props = await serverSideTranslations('en-US', undefined, {
+      i18n: {
+        defaultLocale: 'en-US',
+        locales: ['en-US', 'fr-CA'],
+      },
+    } as UserConfig)
+    expect(fs.readdirSync).toHaveBeenCalledTimes(1)
+    expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/en-US'))
+    expect(Object.values(props._nextI18Next.initialI18nStore))
+      .toEqual([{ one: {}, three: {}, two: {} }])
   })
 
   it('returns props', async () => {
