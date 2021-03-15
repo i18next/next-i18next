957e9c38866e1cf98a093c046717b262f5367fd0
Adriano Raiano <adriano@raiano.ch>
2021-03-15 21:00:49 +0100

    added dedicated test for i18next clone

--- src/createClient/node.test.ts
+++ src/createClient/node.test.ts
@@ -7,8 +7,8 @@ const config = {
 } as any
 
 describe('createClientNode', () => {
+  const client = createClientNode(config)
   it('returns a node client', () => {
-    const client = createClientNode(config)
     expect(typeof client.initPromise.then).toEqual('function')
     expect(typeof client.i18n.addResource).toEqual('function')
     expect(typeof (client.i18n as any).translator).toEqual('object')
@@ -16,5 +16,22 @@ describe('createClientNode', () => {
       (client.i18n.options as any).defaultLocale
     ).toEqual(config.defaultLocale)
     expect((client.i18n.options as any).locales).toEqual(config.locales)
+    expect((client.i18n.options as any).isClone).not.toEqual(true)
+  })
+
+  describe('createClientNode a second time should return a clone of i18next', () => {
+    it('returns a node client', () => {
+      const secondClient = createClientNode(config)
+      expect(typeof secondClient.initPromise.then).toEqual('function')
+      expect(typeof secondClient.i18n.addResource).toEqual('function')
+      expect(typeof (secondClient.i18n as any).translator).toEqual('object')
+      expect(
+        (secondClient.i18n.options as any).defaultLocale
+      ).toEqual(config.defaultLocale)
+      expect((secondClient.i18n.options as any).locales).toEqual(config.locales)
+      expect((secondClient.i18n.options as any).isClone).toEqual(true)
+      expect(secondClient).not.toEqual(client)
+      expect((secondClient as any).store).toEqual((client as any).store)
+    })
   })
 })
