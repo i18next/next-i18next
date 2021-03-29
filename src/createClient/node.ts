6424d1b49bfd3f5edd9704baaed37a34ed1dacf8
Adriano Raiano <adriano@raiano.ch>
2021-03-29 09:02:34 +0200

    fix: get rid of unnecessary i18next instances

--- src/createClient/node.ts
+++ src/createClient/node.ts
@@ -1,32 +1,26 @@
 import i18n from 'i18next'
 import i18nextFSBackend from 'i18next-fs-backend'
 
-import { InternalConfig, CreateClientReturn, InitPromise, I18n } from '../types'
-
-let instance: I18n
+import { InternalConfig, CreateClientReturn, InitPromise } from '../types'
 
 export default (config: InternalConfig): CreateClientReturn => {
-  if (!instance) {
-    instance = i18n.createInstance(config)
-  } else {
-    instance = instance.cloneInstance({
-      ...config,
-      initImmediate: false,
-    })
-  }
   let initPromise: InitPromise
 
-  if (!instance.isInitialized) {
+  if (!i18n.isInitialized) {
     const hasCustomBackend = config?.use?.some((b) => b.type === 'backend')
     if (!hasCustomBackend) {
-      instance.use(i18nextFSBackend)
+      i18n.use(i18nextFSBackend)
     }
 
-    config?.use?.forEach(x => instance.use(x))
-    initPromise = instance.init(config)
+    config?.use?.forEach(x => i18n.use(x))
+    initPromise = i18n.init(config)
   } else {
+    if (i18n.language !== config.lng) {
+      // prevents a next.js warning: "Text content did not match" #1063
+      i18n.changeLanguage(config.lng)
+    }
     initPromise = Promise.resolve(i18n.t)
   }
 
-  return { i18n: instance, initPromise }
+  return { i18n, initPromise }
 }
