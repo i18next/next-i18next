8c8f54471c4abc6de61317fcf264bec392507075
Isaac Hinman <isaac@isaachinman.com>
2021-03-30 11:58:40 +0100

    Update src/createClient/node.ts

--- src/createClient/node.ts
+++ src/createClient/node.ts
@@ -7,7 +7,6 @@ let globalInstance: I18n
 
 export default (config: InternalConfig): CreateClientReturn => {
   let instance: I18n
-  
   if (!globalInstance) {
     globalInstance = i18n.createInstance(config)
     instance = globalInstance
