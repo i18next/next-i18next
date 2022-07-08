834490d4897e646cd75c14d72c378696dbf7f334
Adriano Raiano <adriano@raiano.ch>
2022-07-08 11:09:21 +0200

    docs: hint in example

--- examples/simple/pages/_app.js
+++ examples/simple/pages/_app.js
@@ -1,5 +1,6 @@
 import { appWithTranslation } from 'next-i18next'
+// import nextI18NextConfig from '../next-i18next.config.js'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp/*, nextI18NextConfig */)
