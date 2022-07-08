5c79c2f284028e1ecc33c72ba954cd9d9f4ac389
Adriano Raiano <adriano@raiano.ch>
2022-07-08 11:11:12 +0200

    docs: hint in example

--- examples/simple/pages/_app.js
+++ examples/simple/pages/_app.js
@@ -3,4 +3,5 @@ import { appWithTranslation } from 'next-i18next'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
+// https://github.com/i18next/next-i18next#unserialisable-configs
 export default appWithTranslation(MyApp/*, nextI18NextConfig */)
