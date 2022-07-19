9f13edca44439da4ae658d52ce71e6f29275b323
Adriano Raiano <adriano@raiano.ch>
2022-07-19 14:19:03 +0200

    move reloadOnPrerender to correct place

--- examples/simple/next-i18next.config.js
+++ examples/simple/next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
-    reloadOnPrerender: process.env.NODE_ENV === 'development',
   },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
