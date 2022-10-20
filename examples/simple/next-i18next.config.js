f155b3b5568e7abce2a230fd28eccfc44d489869
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-10-20 22:46:06 +0200

    docs: add comment and link to localePath

--- examples/simple/next-i18next.config.js
+++ examples/simple/next-i18next.config.js
@@ -1,7 +1,4 @@
-// used for SSR (getServerSideProps)
-// const path = require('path')
-// const localePath = path.resolve('./public/locales')
-
+// @link https://github.com/i18next/next-i18next
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   debug: process.env.NODE_ENV === 'development',
