a607fc520e157ecbfed7f2cb4baedf4771b2f21d
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-10-20 22:25:36 +0200

    refactor: add esmExternals to build options

--- examples/simple/next.config.js
+++
@@ -1,5 +0,0 @@
-const { i18n } = require('./next-i18next.config')
-
-module.exports = {
-  i18n,
-}
--- examples/simple/next.config.mjs
+++ examples/simple/next.config.mjs
@@ -1,26 +1,29 @@
 // @ts-check
 
-const trueEnv = ['true', '1', 'yes'];
+import {loadCustomBuildParams} from "./loadCustomBuildParams.mjs";
+
+// @ts-ignore you can remove this when integrating our example.
+const { esmExternals, tsconfigPath } = loadCustomBuildParams();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: trueEnv.includes(
-      process.env?.NEXTJS_SWC_MINIFY ?? 'true'
-  ),
-  typescript: {
-    tsconfigPath: process.env.TSCONFIG_CONFIG_PATH
-        ? process.env.TSCONFIG_CONFIG_PATH
-        : './tsconfig.json',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
   },
+  // localePath,
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
   experimental: {
     // Prefer loading of ES Modules over CommonJS
     // @link {https://nextjs.org/blog/next-11-1#es-modules-support|Blog 11.1.0}
     // @link {https://github.com/vercel/next.js/discussions/27876|Discussion}
-    esmExternals: trueEnv.includes(
-        process.env?.NEXTJS_ESM_EXTERNALS ?? 'false'
-    ),
+    esmExternals
+  },
+  typescript: {
+    tsconfigPath
   },
 };
 
 export default nextConfig;
+
