6595a9109dfa5c3940c733c7674da6f653c3d567
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-10-21 14:06:21 +0200

    fix(example): resolve localePath by default in example

--- examples/simple/next-i18next.config.js
+++ examples/simple/next-i18next.config.js
@@ -10,16 +10,15 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-
   /** To avoid issues when deploying to some paas (vercel...) */
   localePath: typeof window === 'undefined' ?
     require('path').resolve('./public/locales') : '/locales',
 
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+
   /**
    * @link https://github.com/i18next/next-i18next#6-advanced-configuration
    */
-
   // saveMissing: false,
   // strictMode: true,
   // serializeConfig: false,
