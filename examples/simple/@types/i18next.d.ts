746772dfd7f66bfc2b3035690f29f13db42d5644
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-11-05 23:15:48 +0100

    fix: lint

--- examples/simple/@types/i18next.d.ts
+++ examples/simple/@types/i18next.d.ts
@@ -12,14 +12,14 @@ import type footer from '../public/locales/en/footer.json'
 import type secondPage from '../public/locales/en/second-page.json'
 
 interface I18nNamespaces {
-    common: typeof common
-    footer: typeof footer
-    'second-page': typeof secondPage
+  common: typeof common
+  footer: typeof footer
+  'second-page': typeof secondPage
 }
 
 declare module 'i18next' {
-    interface CustomTypeOptions {
-        defaultNS: 'common'
-        resources: I18nNamespaces
-    }
+  interface CustomTypeOptions {
+    defaultNS: 'common'
+    resources: I18nNamespaces
+  }
 }
