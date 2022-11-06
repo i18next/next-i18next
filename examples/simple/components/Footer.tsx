97d01a6d895c0c9e2c826c44f2f8ced96516a791
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-11-06 11:00:39 +0100

    feat: support for version from git

--- examples/simple/components/Footer.tsx
+++ examples/simple/components/Footer.tsx
@@ -3,21 +3,17 @@ import { useTranslation, Trans } from 'next-i18next'
 import type { FC } from 'react'
 
 export const Footer: FC = () => {
-
   const { t } = useTranslation('footer')
+  const { version = 'git' } = pkg as unknown as { version: string }
 
   return (
     <footer>
-      <p>
-        {t('description')}
-      </p>
-      <p>
-        next-i18next v
-        {pkg.version}
-      </p>
-      <p style={{ fontSize: 'smaller', fontStyle: 'italic', marginTop: 20 }}>
+      <p>{t('description')}</p>
+      <p>next-i18next v{version}</p>
+      <p style={{fontSize: 'smaller', fontStyle: 'italic', marginTop: 20}}>
         <Trans i18nKey='helpLocize' t={t}>
-          With using <a href='https://locize.com' target='_new'>locize</a> you directly support the future of <a href='https://www.i18next.com' target='_new'>i18next</a>.
+           With using <a href='https://locize.com' target='_new'>locize</a> you directly support the future
+           of <a href='https://www.i18next.com' target='_new'>i18next</a>.
         </Trans>
       </p>
     </footer>
