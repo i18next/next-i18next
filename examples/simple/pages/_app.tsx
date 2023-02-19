e6b5085b5e92004afa9516bd444b19b2c8cf5758
Broda Noel <brodanoel@gmail.com>
2023-02-19 05:14:16 -0300

    Fix typo (#2092)

--- examples/simple/pages/_app.tsx
+++ examples/simple/pages/_app.tsx
@@ -6,5 +6,5 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 )
 
-// https://github.com/i18next/next-i18next#unserialisable-configs
+// https://github.com/i18next/next-i18next#unserializable-configs
 export default appWithTranslation(MyApp /*, nextI18NextConfig */)
