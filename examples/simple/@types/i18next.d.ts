c419b2be3b4772de17309db474facd6dbeb86888
Adriano Raiano <adriano@raiano.ch>
2023-06-15 11:23:29 +0200

    update examples

--- examples/simple/@types/i18next.d.ts
+++ examples/simple/@types/i18next.d.ts
@@ -7,19 +7,12 @@
  */
 import 'i18next'
 
-import type common from '../public/locales/en/common.json'
-import type footer from '../public/locales/en/footer.json'
-import type secondPage from '../public/locales/en/second-page.json'
-
-interface I18nNamespaces {
-  common: typeof common
-  footer: typeof footer
-  'second-page': typeof secondPage
-}
+// resources.ts file is generated with `npm run toc`
+import resources from './resources.ts'
 
 declare module 'i18next' {
   interface CustomTypeOptions {
     defaultNS: 'common'
-    resources: I18nNamespaces
+    resources: typeof resources
   }
 }
