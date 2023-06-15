d14fc0dd8124af10911ac9d25d0bcd2ef8741c2e
Adriano Raiano <adriano@raiano.ch>
2023-06-15 11:03:27 +0200

    fix ts

--- src/serverSideTranslations.ts
+++ src/serverSideTranslations.ts
@@ -8,7 +8,6 @@ import { globalI18n } from './appWithTranslation'
 
 import { UserConfig, SSRConfig } from './types'
 import { getFallbackForLng, unique } from './utils'
-import { Namespace } from 'i18next'
 
 let DEFAULT_CONFIG_PATH = './next-i18next.config.js'
 
@@ -22,12 +21,11 @@ if (process.env.I18NEXT_DEFAULT_CONFIG_PATH) {
   DEFAULT_CONFIG_PATH = process.env.I18NEXT_DEFAULT_CONFIG_PATH
 }
 
-type ArrayElementOrSelf<T> = T extends Array<infer U> ? U[] : T[]
-
 export const serverSideTranslations = async (
   initialLocale: string,
   namespacesRequired:
-    | ArrayElementOrSelf<Namespace>
+    | string
+    | string[]
     | undefined = undefined,
   configOverride: UserConfig | null = null,
   extraLocales: string[] | false = false
